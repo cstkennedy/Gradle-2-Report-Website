945904b2a0a6a0ae30ff2d3e2e36045e3852568b
Thomas J. Kennedy <tkennedy@cs.odu.edu>
2022-07-18 19:07:48 -0400

    First report

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"edu.odu.cs.cs350.examples.numbers","l":"PrimeGenerator"},{"p":"edu.odu.cs.cs350.examples.numbers","l":"RunPrimeGenerator"}]
